39f155bd0df3f499df54995f91106a2541da3c65
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-16 18:17:41 +0300

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - Prints using printf() command
-* Return: 0 prints phrase
-*/
+ * main - Entry point
+ *
+ * Description: Prints a specific phrase using printf
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+
