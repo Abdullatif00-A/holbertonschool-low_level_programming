1a4cd417fd98e85df6cce5b94756e076662c00f4
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-18 13:06:48 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,29 +1,20 @@
-#include<stdio.h> 
- 
-int main() 
-{ 
- int a,b,c,d,e; 
- 
- printf("ENTER THE FIVE NUMBERS"); 
- scanf("%d %d %d %d %d",&a,&b,&c,&d,&e); 
- 
- if(a>b && a>c &&  a>d && a>e) 
-  printf("%d is largest", a); 
- 
- else 
-  if(b>c && b>d && b>e) 
-   printf("%d is largest", b); 
- 
- else 
-  if(c>d && c>e) 
-   printf("%d is largest", c); 
- 
- else 
-  if(d>e) 
-   printf("%d  is largest", d); 
- 
- else 
-  printf("%d is largest", e); 
-  
- return 0; 
+#include <stdio.h> 
+ /**
+*  main - print decimals 
+*
+* Return: (8) 
+*
+*/
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
 }
