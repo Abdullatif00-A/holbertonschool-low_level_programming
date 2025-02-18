054769eb57bd67fb20b67079b411b37346e098fa
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-18 15:53:50 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,9 @@
-i#include <stdio.h>
- /**
-*  main - print decimals
-*
-* Return: (8)
-*
-*/
-int main(void)
-{
-	char d = '0';
+/**
+ * main - Prints single-digit numbers from 0 to 9
+ *
+ * Description: Uses a while loop to print numbers from 0 to 9
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 
-	while (d <= '9')
-	{
-		putchar(d);
-		d++;
-	}
-	putchar('\n');
-
-	return (0);
-}
