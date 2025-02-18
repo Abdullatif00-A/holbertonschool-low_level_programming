489869b0524b29c6c0ed3a20503c7a86465e3c1c
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-18 16:06:21 +0000

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,22 +3,23 @@
 /**
  * main - Prints single-digit numbers from 0 to 9
  *
- * Description: Uses a while loop to print numbers from 0 to 9
+ * Description: Uses a while loop to print numbers from 0 to 9,
  * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char d = '0';  /* تعريف متغير للحروف من '0' إلى '9' */
+	char d = '0';
 
-    while (d <= '9')  /* التأكد من أن الحرف لم يتجاوز '9' */
-    {
-        putchar(d);  /* طباعة الرقم الحالي */
-        d++;  /* الانتقال إلى الرقم التالي */
-    }
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
 
-    putchar('\n');  /* إضافة سطر جديد بعد طباعة الأرقام */
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
+
