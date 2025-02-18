1e5e9aeb4396c1b99769e1ef5d87ad52a06a66fe
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-18 17:16:04 +0000

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,14 +6,18 @@
  */
 int main(void)
 {
-	int i = 0;
+int num;
 
-	for (i = 10; i < 99; i++)
-	{
-		putchar(i);
-		putchar(',');
-	}
+for (num = 0; num <= 9; num++)
+{
+putchar((num % 10) + '0');
+if (num == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
 
-	putchar('\n');
-	return (0);
+putchar('\n')
+
+return (0);
 }
