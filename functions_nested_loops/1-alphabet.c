4054397ba444e457accdefa89e67c4be4267f5b9
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-20 11:23:45 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*print_alphabet - check the code
-*print alphabet void method 
-*Return: othing.
+* print_alphabet - check the code
+* print alphabet void method
+* Return: nothing.
 */
 void print_alphabet(void)
 {
 char ch;
-for (ch = 'a'; ch <= 'z';ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 _putchar(ch);
 _putchar('\n');
 }
