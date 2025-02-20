b4b897443cd7d188eed93ae596f34991bf123141
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-02-20 13:32:47 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,13 @@
 #include "main.h"
-
 /**
-*print_alphanet_x10 - prints the alphanet 10 times in lowercasw
-*followed by a new line 
+* print_alphabet - check the code
+* print alphabet void method
+* Return: nothing.
 */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-int i;
 char ch;
-
-for (i = 0; i < 10;i++)
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-{
+for (ch = 'a' ; ch <= 'z' ; ch++)
 _putchar(ch);
-}
 _putchar('\n');
-i++;
 }
