446a9fae27afe37fb46a4c8639c5125b0d5e96a0
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-03-17 12:33:08 +0000

    100-hello_world.asm

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,18 +1,17 @@
 section .data
-    msg db "Hello, World", 10
-    len equ $ - msg
+msg db "Hello, Holberton", 9
+len equ $ - msg
 
 section .text
-    global _start
+global _start
 
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
-    syscall
+mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, len
+syscall
 
-    mov rax, 60
-    xor rdi, rdi
-    syscall
-
+mov rax, 60
+xor rdi, rdi
+syscall
