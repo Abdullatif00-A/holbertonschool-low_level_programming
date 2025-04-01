7cbd1775add92c71a6ca69ee9542576652116a77
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-04-01 18:52:40 +0000

    my

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,26 +1,24 @@
-/*
- * File: 0-print_dlistint.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
- * @h: The head of the dlistint_t list.
- *
- * Return: The number of nodes in the list.
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list
+ * Return: the number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+    size_t count = 0;
 
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+        h = h->next;
+        count++;
+    }
 
-	return (nodes);
+    return (count);
 }
