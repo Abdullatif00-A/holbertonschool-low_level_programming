3e1b9314c3df6f118b5d12cd4dfe8fe5a3298626
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-04-01 18:57:45 +0000

    m

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,20 +1,20 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 /**
- * struct listint_s - singly linked list
- * @n: integer
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct listint_s
+typedef struct list_s
 {
-        int n;
-        struct listint_s *next;
-} listint_t;
-
-size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 #endif /* LISTS_H */
