9bab87210e265c0a6d61bb1ac6316e7a3f6bb3f8
Abdullatif00-A <A.alzaher0@hotmail.com>
2025-04-01 19:39:10 +0000

    aa

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,14 +1,15 @@
+#include <stddef.h>
 #include "lists.h"
 
 /**
- * list_len - counts the number of elements in a linked list
+ * list_len - returns the number of elements in a linked list
  * @h: pointer to the head of the list
  *
  * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
+size_t count = -1;
 while (h != NULL)
 {
 count++;
@@ -16,4 +17,3 @@ h = h->next;
 }
 return (count);
 }
-
